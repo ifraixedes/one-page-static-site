c06044a1bf98a5742f9398b03dece7035039294f
Ivan Fraixedes <ivan@fraixed.es>
2019-01-02 19:00:25 +0100

    cmd/opss: Move the only bin sources to subpkg
    Move the only binary package (cmd) to a subpackage for having a better
    default name for the binary generated by for go build.

--- cmd/main.go
+++
@@ -1,7 +0,0 @@
-package main // import "go.fraixed.es/onepagestaticsite/cmd"
-
-import "go.fraixed.es/onepagestaticsite/cmd/internal"
-
-func main() {
-	internal.Execute()
-}
---
+++ cmd/opss/main.go
@@ -0,0 +1,7 @@
+package main // import "go.fraixed.es/onepagestaticsite/cmd/opss"
+
+import "go.fraixed.es/onepagestaticsite/cmd/opss/internal"
+
+func main() {
+	internal.Execute()
+}
